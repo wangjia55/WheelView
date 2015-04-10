76516a0f0f9e7101bff94932e32103b6e99b7a19
jacob-wj <wangjia199055@126.com>
2015-04-10 15:48:06 +0800

    增加了注释

--- app/src/main/java/com/jacob/wheelview/WheelView.java
+++ app/src/main/java/com/jacob/wheelview/WheelView.java
@@ -23,20 +23,40 @@ import java.util.List;
  * Created by jacob-wj on 2015/4/10.
  */
 public class WheelView extends ScrollView {
-
+    /**
+     * 绘制分割线的画笔
+     */
     private Paint mPaintBorder;
 
+    /**
+     * 在ScrollView中使用Linearlayout存放Item
+     */
     private LinearLayout mLinearContainer;
 
+    /**
+     * 每个item的高度
+     */
     private int mItemHeight;
 
+    /**
+     * 每个item的宽度
+     */
     private int mItemWidth;
     private List<String> mItems = new ArrayList<>();
 
+    /**
+     * 中心点上下各显示多少个，
+     */
     private int mOffset;
 
+    /**
+     * 当前选中的位置
+     */
     private int mSelection;
 
+    /**
+     * 显示item的个数
+     */
     private int mDisplayCount;
     private int[] mBorderHeight = new int[2];
 
@@ -81,7 +101,6 @@ public class WheelView extends ScrollView {
         mLinearContainer = new LinearLayout(getContext());
         mLinearContainer.setOrientation(LinearLayout.VERTICAL);
         addView(mLinearContainer);
-
     }
 
     @Override
@@ -147,6 +166,9 @@ public class WheelView extends ScrollView {
         refreshSelectedUI(0);
     }
 
+    /**
+     * 更新中间选中的文字的颜色和大小
+     */
     private void refreshSelectedUI(int y) {
         int position = y / mItemHeight + mOffset;
         int remain = y % mItemHeight;
@@ -172,7 +194,7 @@ public class WheelView extends ScrollView {
     }
 
     /**
-     * she
+     * 设置默认选择的位置
      */
     public void setSelection(int selection) {
         final int index = selection;
@@ -185,6 +207,9 @@ public class WheelView extends ScrollView {
         });
     }
 
+    /**
+     * 设置偏移量
+     */
     public void setOffset(int offset) {
         this.mOffset = offset;
         this.mDisplayCount = mOffset * 2 + 1;
@@ -204,6 +229,9 @@ public class WheelView extends ScrollView {
         mBorderHeight[1] = mItemHeight * (mOffset + 1);
     }
 
+    /**
+     * 创建TextView，并且设置父布局的尺寸
+     */
     private TextView createView(String title) {
         TextView textView = new TextView(getContext());
         textView.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
@@ -226,6 +254,9 @@ public class WheelView extends ScrollView {
         return textView;
     }
 
+    /**
+     * 测量每个item的高度（重点）
+     */
     private int measureItemHeight(View view) {
         int childWidthSpec;
         int childHeightSpec;
@@ -247,12 +278,17 @@ public class WheelView extends ScrollView {
     }
 
 
+    /**
+     * 设置滑动的速度，这里速度放慢
+     */
     @Override
     public void fling(int velocityY) {
         super.fling(velocityY / 3);
     }
 
-
+    /**
+     * 当触摸结束后，重新计算位置
+     */
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         switch (ev.getAction()) {
@@ -305,6 +341,9 @@ public class WheelView extends ScrollView {
     }
 
 
+    /**
+     * 滑动事件的回调
+     */
     public interface OnWheelPickerListener {
         void wheelSelect(int position, String content);
     }
